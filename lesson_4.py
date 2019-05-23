bbb17513e90a143a702fac5cae0dea213f54ce4c
AlfaBettaGamma <surmenko_konstantin@mail.ru>
2019-05-23 10:30:39 +0300

    update

--- lesson_4.py
+++ lesson_4.py
@@ -8,21 +8,21 @@ def ConquestCampaign(N, M, L, battalion):
   lastm = []
   p2 = 0
   d = 2
-  if(L*2 == len(battalion)):
+  if L*2 == len(battalion) :
     for i in range(len(battalion)):
-      if(i == 0):
+      if i == 0 :
         n.append(battalion[i])
-      elif(i == 1):
+      elif i == 1 :
         m.append(battalion[i])
       else:
-        if(i%2 == 0):
+        if i%2 == 0 :
           n.append(battalion[i]) 
         else:
           m.append(battalion[i])
     for i in range(len(n)):
       if n[i] > N or m[i] > M :
         return print('Введенные значения выходят за рамки матрицы')
-    for i in range(L): #проверка на повторы
+    for i in range(L):
       p.clear()
       p.append(n[i])
       p.append(m[i])
@@ -39,33 +39,33 @@ def ConquestCampaign(N, M, L, battalion):
       m.remove(0)
       L -= 1
     while N*M is not L:
-        if(d <= 2 and L <= N*M):
-            for i in range(L): #захват
-              if(1 <= n[i] <= N and 1 <= m[i] - 1 <= M ):
+        if d <= 2 and L <= N*M :
+            for i in range(L):
+              if 1 <= n[i] <= N and 1 <= m[i] - 1 <= M  :
                 n.append(n[i])
                 m.append(m[i]-1)
                 newn.append(n[i])
                 newm.append(m[i] -1)
                 L += 1
-              if(1 <= n[i] + 1 <= N and 1 <= m[i] <= M ):
+              if 1 <= n[i] + 1 <= N and 1 <= m[i] <= M  :
                 n.append(n[i]+1)
                 m.append(m[i])
                 newn.append(n[i]+1)
                 newm.append(m[i])                
                 L += 1
-              if(1 <= n[i] <= N and 1 <= m[i] + 1 <= M ):
+              if 1 <= n[i] <= N and 1 <= m[i] + 1 <= M  :
                 n.append(n[i])
                 m.append(m[i]+1)
                 newn.append(n[i])
                 newm.append(m[i]+1)
                 L += 1
-              if(1 <= n[i] - 1 <= N and 1 <= m[i] <= M ):
+              if 1 <= n[i] - 1 <= N and 1 <= m[i] <= M  :
                 n.append(n[i]-1)
                 m.append(m[i])
                 newn.append(n[i]-1)
                 newm.append(m[i])
                 L += 1
-            for i in range(len(n)): #проверка на повторы
+            for i in range(len(n)):
               p.clear()
               p.append(n[i])
               p.append(m[i])
@@ -86,33 +86,33 @@ def ConquestCampaign(N, M, L, battalion):
             for i in range(len(n)):
                 battalion.append(n[i])
                 battalion.append(m[i])
-        elif(d%2 == 1 and L <= N*M):
-            for i in range(len(newn)): #захват
-              if(1 <= newn[i] <= N and 1 <= (newm[i] - 1) <= M ):
+        elif d%2 == 1 and L <= N*M :
+            for i in range(len(newn)):
+              if 1 <= newn[i] <= N and 1 <= (newm[i] - 1) <= M  :
                 n.append(newn[i])
                 m.append(newm[i]-1)
                 lastn.append(newn[i])
                 lastm.append(newm[i] -1)
                 L += 1
-              if(1 <= (newn[i] + 1) <= N and 1 <= newm[i] <= M ):
+              if 1 <= (newn[i] + 1) <= N and 1 <= newm[i] <= M  :
                 n.append(newn[i]+1)
                 m.append(newm[i])
                 lastn.append(newn[i]+1)
                 lastm.append(newm[i])                
                 L += 1
-              if(1 <= newn[i] <= N and 1 <= (newm[i] + 1) <= M ):
+              if 1 <= newn[i] <= N and 1 <= (newm[i] + 1) <= M  :
                 n.append(newn[i])
                 m.append(newm[i]+1)
                 lastn.append(newn[i])
                 lastm.append(newm[i]+1)
                 L += 1
-              if(1 <= (newn[i] - 1) <= N and 1 <= newm[i] <= M ):
+              if 1 <= (newn[i] - 1) <= N and 1 <= newm[i] <= M  :
                 n.append(newn[i]-1)
                 m.append(newm[i])
                 lastn.append(newn[i]-1)
                 lastm.append(newm[i])
                 L += 1
-            for i in range(len(n)): #проверка на повторы
+            for i in range(len(n)):
               p.clear()
               p.append(n[i])
               p.append(m[i])
@@ -128,7 +128,7 @@ def ConquestCampaign(N, M, L, battalion):
               n.remove(0)
               m.remove(0)
               L -= 1
-            for i in range(len(lastn)): #проверка на повторы
+            for i in range(len(lastn)):
               p.clear()
               p.append(lastn[i])
               p.append(lastm[i])
@@ -150,33 +150,33 @@ def ConquestCampaign(N, M, L, battalion):
             for i in range(len(n)):
                 battalion.append(n[i])
                 battalion.append(m[i])
-        elif(d%2 == 0 and L <= N*M):
-            for i in range(len(lastn)): #захват
-              if(1 <= lastn[i] <= N and 1 <= (lastm[i] - 1) <= M ):
+        elif d%2 == 0 and L <= N*M :
+            for i in range(len(lastn)):
+              if 1 <= lastn[i] <= N and 1 <= (lastm[i] - 1) <= M  :
                 n.append(lastn[i])
                 m.append(lastm[i]-1)
                 newn.append(lastn[i])
                 newm.append(lastm[i]-1)
                 L += 1
-              if(1 <= (lastn[i] + 1) <= N and 1 <= lastm[i] <= M ):
+              if 1 <= (lastn[i] + 1) <= N and 1 <= lastm[i] <= M  :
                 n.append(lastn[i]+1)
                 m.append(lastm[i])
                 newn.append(lastn[i]+1)
                 newm.append(lastm[i])                
                 L += 1
-              if(1 <= lastn[i] <= N and 1 <= (lastm[i] + 1) <= M ):
+              if 1 <= lastn[i] <= N and 1 <= (lastm[i] + 1) <= M  :
                 n.append(lastn[i])
                 m.append(lastm[i]+1)
                 newn.append(lastn[i])
                 newm.append(lastm[i]+1)
                 L += 1
-              if(1 <= (lastn[i] - 1) <= N and 1 <= lastm[i] <= M ):
+              if 1 <= (lastn[i] - 1) <= N and 1 <= lastm[i] <= M  :
                 n.append(lastn[i]-1)
                 m.append(lastm[i])
                 newn.append(lastn[i]-1)
                 newm.append(lastm[i])
                 L += 1
-            for i in range(len(n)): #проверка на повторы
+            for i in range(len(n)):
               p.clear()
               p.append(n[i])
               p.append(m[i])
@@ -192,7 +192,7 @@ def ConquestCampaign(N, M, L, battalion):
               n.remove(0)
               m.remove(0)
               L -= 1
-            for i in range(len(newn)): #проверка на повторы
+            for i in range(len(newn)):
               p.clear()
               p.append(newn[i])
               p.append(newm[i])
